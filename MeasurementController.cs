8cacca598c42681606f4b82e7baab5ca361ef66c
Alun Williams <alun.wiliams@gmail.com>
2019-12-01 20:36:06 +0000

    Initial

---
+++ MeasurementController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace house_dashboard_server
+{
+    [ApiController]
+    [Route("measurement")]
+    public class MeasurementController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "HELLO";
+        }
+    }
+}
