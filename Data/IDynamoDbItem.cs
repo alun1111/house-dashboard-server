57b90df4eba47fcae5f4b8493f1fa0dd8244a823
Alun Williams <alun.williams@gmail.com>
2020-05-05 21:48:01 +0100

    Add TimeIndex interface

--- Data/IDynamoDbItem.cs
+++ Data/IDynamoDbItem.cs
@@ -5,6 +5,7 @@ namespace house_dashboard_server.Data
     public interface IDynamoDbItem<T>
     {
         DateTime MeasurementTime { get; }
+        long TimeIndex { get; }
         T Value { get; }
     }
 }
